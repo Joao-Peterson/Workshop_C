d9af13e08c0ea079650b147da8e96ebbe92d10d1
peterson <jooxpeters@gmail.com>
2021-10-28 00:38:55 -0300

    project addd

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 #include <stdint.h>
 #include "imprime.h"
 
+int calcula_temperatura_int(void);
+
+#define calcula_temperatura(type) (type)calcula_temperatura_int()
+
+
 int main(){
+
+    funcao_critica();
+
+    int temperatura = calcula_temperatura(int);
 
     // imprime("Hello world!");
 
